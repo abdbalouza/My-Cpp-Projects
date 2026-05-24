9cc73c735509ab1cb51f03f645eef88727991d12
abdbalouza <abd.balouza@gmail.com>
2026-05-24 21:59:02 +0200

    Initial upload of my c++ projects

---
+++ Source.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int a, b, ans;
+	cin >> a >> b;
+	for (int i = 1; i <= a; i++)
+		if (a % i == 0 && b % i == 0)
+			ans = i;
+	cout << ans << endl;
+	return 0;
+}
